5eb0a0ea95a4786c00a0730a38ffef26f5810f39
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-09-06 22:54:40 +0300

    Add CRUD API

---
+++ services/functions/get.js
@@ -0,0 +1,22 @@
+import handler from "../utils/handler";
+import dynamoDB from "../utils/dynamodb";
+
+export const main = handler(async (event) => {
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Key: {
+      // The attributes of the item to be created
+      userId: "123", // The id of the author
+      noteId: event.pathParameters.id,
+    },
+  };
+
+  const result = await dynamoDB.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+
+});
