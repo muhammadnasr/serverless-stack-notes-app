050c1f90e785b7fb1a91ba70bbd0e324e3e9eaa4
Muhammad Hassan Nasr <muhammad.hassan.nasr@gmail.com>
2022-09-08 15:03:14 +0300

    add CORS to lambda and s3

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -10,8 +10,17 @@ export function StorageStack({ stack, app }) {
     primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
   });
 
-  const bucket = new Bucket(stack, "Uploads");
-
+  const bucket = new Bucket(stack, "Uploads", {
+    cors: [
+      {
+        maxAge: "1 day",
+        allowedOrigins: ["*"],
+        allowedHeaders: ["*"],
+        allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+      },
+    ],
+  });
+  
   return {
     table,
     bucket
